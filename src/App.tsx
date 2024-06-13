9f1d279da817ab6c7c69c9212de68158b794871e
Clarence Luk <luk.clarence@gmail.com>
2024-06-13 01:33:38 -0700

    added dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,61 @@
 import styles from './App.module.css';
-import { Box, Menu, MenuItem, ThemeProvider, Typography } from '@mui/material';
+import {
+  Box,
+  CssBaseline,
+  Menu,
+  MenuItem,
+  Switch,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
 import Background from './background/background';
+import { useState } from 'react';
+
+import { createTheme } from '@mui/material/styles';
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#1976d2',
+    },
+    secondary: {
+      main: '#dc004e',
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#90caf9',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  },
+});
 
 const App = () => {
-  const theme = {
-    palette: {
-      primary: {
-        main: '#007FFF',
-        dark: '#0066CC',
-      },
-    },
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  const handleThemeChange = (event: {
+    target: { checked: boolean | ((prevState: boolean) => boolean) };
+  }) => {
+    setIsDarkMode(event.target.checked);
   };
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
+      <CssBaseline />
       <Box className={styles.appDiv} justifyContent="space-between" p={2}>
         <Menu open={true}>
           <MenuItem>Home</MenuItem>
           <MenuItem>App 1</MenuItem>
+          <MenuItem>
+            <Switch checked={isDarkMode} onChange={handleThemeChange} />
+          </MenuItem>
         </Menu>
         <Typography variant="h1">Home</Typography>
         <Background />
