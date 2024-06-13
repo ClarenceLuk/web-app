7e0ba4739be95f3d1878a5c0fdf99e9f0c443935
Clarence Luk <luk.clarence@gmail.com>
2024-06-13 01:34:13 -0700

    added basic background shapes

---
+++ src/background/background.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Rect, Stage, Layer, Circle, Line, Text } from 'react-konva';
+
+interface Props {}
+
+const Background = () => {
+  return (
+    <div>
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <Text text="Some text on canvas" fontSize={15} />
+          <Rect
+            x={20}
+            y={50}
+            width={100}
+            height={100}
+            fill="red"
+            shadowBlur={10}
+          />
+          <Circle x={200} y={100} radius={50} fill="green" />
+          <Line
+            x={20}
+            y={200}
+            points={[0, 0, 100, 0, 100, 100]}
+            tension={0.5}
+            closed={true}
+            stroke="black"
+            fillLinearGradientStartPoint={{ x: -50, y: -50 }}
+            fillLinearGradientEndPoint={{ x: 50, y: 50 }}
+            fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
+          />
+        </Layer>
+      </Stage>
+    </div>
+  );
+};
+
+export default Background;
