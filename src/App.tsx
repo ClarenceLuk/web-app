c48d60a8a74bf93a71e55f54b3dbe8ae8716b904
Clarence Luk <luk.clarence@gmail.com>
2024-06-13 01:16:26 -0700

    added a basic shapes for background template

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import styles from './App.module.css';
-import { Menu, MenuItem, Typography } from '@mui/material';
+import { Box, Menu, MenuItem, ThemeProvider, Typography } from '@mui/material';
+import Background from './background/background';
 
 const App = () => {
+  const theme = {
+    palette: {
+      primary: {
+        main: '#007FFF',
+        dark: '#0066CC',
+      },
+    },
+  };
   return (
-    <div className={styles.appDiv}>
-      <Menu open={true}>
-        <MenuItem>Home</MenuItem>
-        <MenuItem>App 1</MenuItem>
-      </Menu>
-      <Typography variant="h1">Home</Typography>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Box className={styles.appDiv} justifyContent="space-between" p={2}>
+        <Menu open={true}>
+          <MenuItem>Home</MenuItem>
+          <MenuItem>App 1</MenuItem>
+        </Menu>
+        <Typography variant="h1">Home</Typography>
+        <Background />
+      </Box>
+    </ThemeProvider>
   );
 };
 
