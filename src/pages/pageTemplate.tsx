8e291dc83fea0a942abe5eb720869151a0a5cb23
Clarence Luk <luk.clarence@gmail.com>
2024-06-21 09:59:13 -0700

    renamed a few files and hooks

---
+++ src/pages/pageTemplate.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+
+interface PageProps {
+  title: string
+  children: JSX.Element
+}
+
+const PageTemplate = ({ title, children }: PageProps) => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100vh',
+      }}>
+      {/* Title Area */}
+      <Box
+        sx={{
+          padding: 2,
+          backgroundColor: 'primary.main',
+          color: 'primary.contrastText',
+        }}>
+        <Typography variant="h4">{title}</Typography>
+      </Box>
+
+      {/* Body Area */}
+      <Box
+        sx={{
+          flexGrow: 1,
+          padding: 2,
+          overflowY: 'auto', // Allows scrolling if content overflows
+        }}>
+        {children}
+      </Box>
+    </Box>
+  )
+}
+
+export default PageTemplate
