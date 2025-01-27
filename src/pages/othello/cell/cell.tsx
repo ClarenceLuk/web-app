571f5c598ccdacf0f2bcb40c20ea64e1387dbe3a
Clarence Luk <luk.clarence@gmail.com>
2025-01-26 19:19:48 -0800

    added styles for black/white/valid moves

--- src/pages/othello/cell/cell.tsx
+++ src/pages/othello/cell/cell.tsx
@@ -2,14 +2,30 @@ import React from "react";
 import styles from "./cell.module.css";
 
 interface CellProps {
-  value: string; 
+  value: string; // 'B' for black, 'W' for white, or empty string
   row: number;
   col: number;
 }
 
-
 const Cell: React.FC<CellProps> = ({ value, row, col }) => {
-  return <div className={styles.cell} />
-}
+  const pieceClass = React.useMemo(() => {
+    switch (value) {
+      case "B":
+        return styles.black;
+      case "W":
+        return styles.white;
+      case "V":
+        return styles.valid;
+      default:
+        return "";
+    }
+  }, [value]); // Recalculate only when value changes
+
+  return (
+    <div className={styles.cell} key={`${value}-${row}-${col}`}>
+      <div className={pieceClass} />
+    </div>
+  );
+};
 
 export default Cell;
--- src/pages/othello/othello.tsx
+++ src/pages/othello/othello.tsx
@@ -4,6 +4,10 @@ import styles from "./othello.module.css"
 
 const Othello = () => {
   const board: string[][] = Array.from({ length: 8 }, () => Array(8).fill(''));
+  board[4][4] = 'B'
+  board[3][3] = 'B'
+  board[4][3] = 'W'
+  board[3][4] = 'W'
 
   return (
     <div className={styles.board}>
