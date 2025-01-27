6cf815f9e8c457193c982826ce050846409577e0
Clarence Luk <luk.clarence@gmail.com>
2025-01-27 12:22:38 -0800

    removed unused imports

--- src/pages/othello/othello.tsx
+++ src/pages/othello/othello.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import React from "react";
 import Cell from "./cell/cell";
 import styles from "./othello.module.css"
