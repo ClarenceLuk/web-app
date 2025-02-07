fd34d3eab99a88680574d43b912c17eaf315c4bd
Clarence Luk <luk.clarence@gmail.com>
2025-02-07 15:05:56 -0800

    organized styling into gameStats.module.css and formatted code styling

--- src/pages/othello/gameStats.tsx
+++ src/pages/othello/gameStats.tsx
@@ -10,21 +10,22 @@ interface GameStatsProps {
 }
 
 const GameStats = ({gameState, handleReset}: GameStatsProps) => {
-  return <Box className={styles.gameUI}>
-  <Button variant="outlined" onClick={handleReset}>
-    Reset
-  </Button>
-  <Box style={{display:'flex', flexDirection:'row', alignItems:'center'}}>
-    <Typography variant="subtitle2">Turn:</Typography>
-    <Box className={cellStyles[gameState.player]} />
+  return (
+  <Box className={styles.gameUI}>
+    <Button variant="outlined" onClick={handleReset}>
+      Reset
+    </Button>
+    <Box className={styles.turn}>
+      <Typography variant="subtitle2">Turn:</Typography>
+      <Box className={cellStyles[gameState.player]} />
+    </Box>
+    <Typography variant="subtitle2">
+      Black: {gameState.chipCounts.black}
+    </Typography>
+    <Typography variant="subtitle2">
+      White: {gameState.chipCounts.white}
+    </Typography>
   </Box>
-  <Typography variant="subtitle2">
-    Black: {gameState.chipCounts.black}
-  </Typography>
-  <Typography variant="subtitle2">
-    White: {gameState.chipCounts.white}
-  </Typography>
-</Box>
-}
+)}
 
 export default GameStats
